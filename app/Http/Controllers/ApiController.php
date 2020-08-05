01ce2cb9a2525f44170604ffe0d9998edf1ca1e2
David Guerra <david.guerra@ingenio-se.com>
2020-08-04 22:47:45 -0500

    08049

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -402,7 +402,7 @@ class ApiController extends Controller
         
         $tipoDoc = $d->tipo_documento_afiliado;
         $numDoc = $d->num_documento_afiliado;
-        $response = json_decode($this->validacion($tipoDoc,$numDoc));
+        $response = $this->validacion($tipoDoc,$numDoc);
         //dd(gettype($response));
         $afiliado = $response->responseMessageOut->body->response->validadorResponse->DsAfiliado->Afiliado;
         if (is_array($afiliado) == false) {
