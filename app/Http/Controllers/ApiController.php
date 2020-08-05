632a8d734e58ee45b9f6a442429eb463dbe014ac
David Guerra <david.guerra@ingenio-se.com>
2020-08-04 22:38:12 -0500

    08046

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -403,7 +403,7 @@ class ApiController extends Controller
         $tipoDoc = $d->tipo_documento_afiliado;
         $numDoc = $d->num_documento_afiliado;
         $response = json_decode($this->validacion($tipoDoc,$numDoc));
-        //dd($response);
+        dd(gettype($response));
         $afiliado = $response->responseMessageOut->body->response->validadorResponse->DsAfiliado->Afiliado;
         if (is_array($afiliado) == false) {
             $afiliado = array($afiliado);
@@ -517,7 +517,7 @@ class ApiController extends Controller
         $tipoDoc = $d->tipo_documento_afiliado;
         $numDoc = $d->num_documento_afiliado;
         $response = json_decode($this->IncapacidadController->validacion($tipoDoc,$numDoc));
-        //dd($response);
+        dd(gettype($response));
         $afiliado = $response->responseMessageOut->body->response->validadorResponse->DsAfiliado->Afiliado;
         if (is_array($afiliado) == false) {
             $afiliado = array($afiliado);
