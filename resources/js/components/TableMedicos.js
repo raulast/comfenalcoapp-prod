0cc24a21f0ae872023187a2adc1675a8f4833128
David Guerra <david.guerra@ingenio-se.com>
2020-12-19 10:56:39 -0500

    202012-19

--- resources/js/components/TableMedicos.js
+++ resources/js/components/TableMedicos.js
@@ -11,13 +11,7 @@ export default function TableMedicos(props) {
         props.handleEliminar(u.target.id)
     }
     const editar = (u) =>{
-<<<<<<< HEAD:resources/js/containers/Medicos/TableMedicos.js
-        console.log(u.target.name);
-        const name = u.target.name.split('/');
-       
-=======
         const name = u.target.name.split('/')
->>>>>>> 63338422eb5b99c43aa38e734f2e1f19859e5a45:resources/js/components/TableMedicos.js
         props.handleEdition(u.target.id, name)
     }
     const medicos = props.medicos;
@@ -42,13 +36,8 @@ export default function TableMedicos(props) {
                             medicos[key]['reg_medico']+'/'+
                             medicos[key]['especialidad']
                         }
-<<<<<<< HEAD:resources/js/containers/Medicos/TableMedicos.js
-                        onClick={editar}><span><i className="far fa-edit"/></span></button></td>
-                    <td><button className="btn btn-danger btn-sm" id={medicos[key]['id']} onClick={eliminar}><i className="fas fa-trash-alt"></i></button></td>
-=======
                         onClick={editar}>Editar</button></td>
                     {/*<td><button className="btn btn-danger btn-sm" id={medicos[key]['id']} onClick={eliminar}>Eliminar</button></td>*/}
->>>>>>> 63338422eb5b99c43aa38e734f2e1f19859e5a45:resources/js/components/TableMedicos.js
                 </tr>
             ))}
         </tbody>
