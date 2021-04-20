75ecbb83aa96e1eee2e16f05000db452fd59dbd4
Daniel Arrubla <darrubla@uninorte.edu.co>
2021-04-20 08:09:46 -0500

    revisión botón guardar Incapacidad

--- resources/js/components/Incapacidad.js
+++ resources/js/components/Incapacidad.js
@@ -789,7 +789,6 @@ class IncapacidadFront extends Component {
     }
     async guardarIncapacidad({target}){
         target.disabled = true;
-        console.log('target buttonClicked: ', target.disabled);
         //console.log(this.state)
         //console.log(parseInt(this.state.diasSolicitados));
         var esp = "otros"
@@ -808,11 +807,10 @@ class IncapacidadFront extends Component {
                         let url = 'saveIncapacidad'
                         axios.post(url, { datos: this.state })
                             .then(resp => {
-                                target.disabled = false;
                                 console.log(resp.data)
                                 // alert(resp.data)
                                 this.handleToast(resp.data,'success');
-                                target.disabled = false;
+                                setTimeout(target.disabled = false,1500);
                                 //this.setState(this.initialState);
                             // location.reload();
                             this.setState({
@@ -822,13 +820,13 @@ class IncapacidadFront extends Component {
                             .catch(err => {
                                 console.log(err)
                                 this.handleToast(err,'error');
-                                target.disabled = false;
+                                setTimeout(target.disabled = false,1500);
                             })
                     }
                     else{
                         // alert("Hay errores en algunos campos");
                         this.handleToast("Hay errores en algunos campos",'error');
-                        target.disabled = false;
+                        setTimeout(target.disabled = false,1500);
                     }
             }
             else{
@@ -849,7 +847,7 @@ class IncapacidadFront extends Component {
                                 console.log(resp.data)
                                 // alert(resp.data)
                                 this.handleToast(resp.data,'success');
-                                target.disabled = false;
+                                setTimeout(target.disabled = false,1500);
                                 //this.setState(this.initialState);
                             // location.reload();
                             this.setState({
@@ -859,13 +857,13 @@ class IncapacidadFront extends Component {
                             .catch(err => {
                                 console.log(err)
                                 this.handleToast(err,'error');
-                                target.disabled = false;
+                                setTimeout(target.disabled = false,1500);
                             })
                     }
                     else{
                         // alert("Hay errores en algunos campos 2");
                         this.handleToast("Hay errores en algunos campos",'error');
-                        target.disabled = false;
+                        setTimeout(target.disabled = false,1500);
                     }
         }
     }
